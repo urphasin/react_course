bebb3ffeedf11ef4dee7ab8601de6b911a647cd8
otito <otitoamuga@gmail.com>
2025-05-19 22:53:16 -0400

    fixes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        outDir: 'dist-react',
+    },
+    base: './'
 })
